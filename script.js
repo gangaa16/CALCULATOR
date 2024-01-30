f60f65eef57bf8ea022b1f83f253a0154464e4ae
Ganga T Dharan <gangatdharan16@gmail.com>
2024-01-30 19:25:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let text =document.getElementById('text');
-function btnClick(value){
-    text.value +=value;
+function btnClick(value) {
+    if (['+', '-', '*', '/', '%'].includes(value) && ['+', '-', '*', '/', '%'].includes(text.value.slice(-1))) {
+
+        return;
+    }
+    if (".".includes(value) && '.'.includes(text.value.slice(-1))){
+        return
+    }
+    text.value += value;
+
 }
 
 function result(){
